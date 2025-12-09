0b094939b6808eb2af1260c880a50c49ea6c6d9e
Nahel <etudiant@Haut-Potentiel.>
2025-12-09 14:14:58 +0100

    git init

--- Type/ArrayType.java
+++ Type/ArrayType.java
@@ -22,33 +22,32 @@ public class ArrayType extends Type{
 
     @Override
     public Map<UnknownType, Type> unify(Type t) {
-        if (t instanceof ArrayType) {
-            return tabType.unify(((ArrayType) t).tabType);
-        }
-        if (t instanceof UnknownType) {
-            return Map.of((UnknownType) t, this);
-        }
-        return null;
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'unify'");
     }
 
     @Override
     public Type substitute(UnknownType v, Type t) {
-        return new ArrayType(tabType.substitute(v, t));
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'substitute'");
     }
 
     @Override
     public boolean contains(UnknownType v) {
-        return tabType.contains(v);
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
     }
 
     @Override
-    public boolean equals(Object o) {
-        return o instanceof ArrayType && tabType.equals(((ArrayType) o).tabType);
+    public boolean equals(Object t) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'equals'");
     }
 
     @Override
     public String toString() {
-        return tabType + "[]";
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
     }
 
     
--- Type/FunctionType.java
+++ Type/FunctionType.java
@@ -1,6 +1,5 @@
 package Type;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 public class FunctionType extends Type {
@@ -44,85 +43,32 @@ public class FunctionType extends Type {
 
     @Override
     public Map<UnknownType, Type> unify(Type t) {
-        if (!(t instanceof FunctionType)) {
-            if (t instanceof UnknownType) {
-                return Map.of((UnknownType) t, this);
-            }
-            return null;
-        }
-
-        FunctionType ft = (FunctionType) t;
-        if (argsTypes.size() != ft.argsTypes.size()) return null;
-
-        Map<UnknownType, Type> subst = new HashMap<>();
-
-        // Unifier les paramètres
-        for (int i = 0; i < argsTypes.size(); i++) {
-            Map<UnknownType, Type> s = argsTypes.get(i).unify(ft.argsTypes.get(i));
-            if (s == null) return null;
-            subst = compose(subst, s);
-        }
-
-        // Unifier le retour
-        Type ret1 = returnType.substituteAll(subst);
-        Type ret2 = ft.returnType.substituteAll(subst);
-        Map<UnknownType, Type> sret = ret1.unify(ret2);
-        if (sret == null) return null;
-        subst = compose(subst, sret);
-
-        return subst;
-    }
-
-    // Utilitaire : compose deux substitutions
-    private Map<UnknownType, Type> compose(Map<UnknownType, Type> s1, Map<UnknownType, Type> s2) {
-        Map<UnknownType, Type> result = new HashMap<>(s1);
-        for (var e : s2.entrySet()) {
-            Type t = e.getValue().substituteAll(s1);
-            result.put(e.getKey(), t);
-        }
-        return result;
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'unify'");
     }
 
     @Override
     public Type substitute(UnknownType v, Type t) {
-        ArrayList<Type> newArgs = new ArrayList<>();
-        for (Type arg : argsTypes) {
-            newArgs.add(arg.substitute(v, t));
-        }
-        return new FunctionType(returnType.substitute(v, t), newArgs);
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'substitute'");
     }
 
     @Override
     public boolean contains(UnknownType v) {
-        if (returnType.contains(v)) return true;
-        for (Type arg : argsTypes) {
-            if (arg.contains(v)) return true;
-        }
-        return false;
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof FunctionType)) return false;
-        FunctionType ft = (FunctionType) o;
-        if (argsTypes.size() != ft.argsTypes.size()) return false;
-        if (!returnType.equals(ft.returnType)) return false;
-        for (int i = 0; i < argsTypes.size(); i++) {
-            if (!argsTypes.get(i).equals(ft.argsTypes.get(i))) return false;
-        }
-        return true;
+    public boolean equals(Object t) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'equals'");
     }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("(");
-        for (int i = 0; i < argsTypes.size(); i++) {
-            sb.append(argsTypes.get(i));
-            if (i < argsTypes.size() - 1) sb.append(", ");
-        }
-        sb.append(") -> ").append(returnType);
-        return sb.toString();
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
     }
 
 }
